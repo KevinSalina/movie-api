f2eb8d1341d0e18b44b9f1734e18d4d441f6ff1d
Kevin Salina <kevin.salina25@gmail.com>
2021-08-11 19:20:00 -0400

    Moved routing functions to controller folder

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,5 +4,38 @@ const getAllMovies = (req, res) => {
   res.send(movies)
 }
 
+const searchByTitleAndDirector = (req, res) => {
+  const qSearch = req.params.query.toLowerCase()
 
-module.exports = { getAllMovies }
+  const movieResults = movies.filter(movie => movie.title.toLowerCase().includes(qSearch))
+
+  const directorResults = movies.filter(movie => {
+    for (director of movie.directors) {
+      if (director.toLowerCase().includes(qSearch)) {
+        return movie
+      }
+    }
+  })
+
+  const combinedResults = movieResults.concat(directorResults)
+
+  res.send(combinedResults)
+
+}
+
+const createNewMovie = (req, res) => {
+  const { title, directors, releaseDate, rating, runTime, genres } = req.body
+
+  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
+    return res.status(400).send('Missing Movie Data!!')
+  }
+
+  const newMovie = { title, directors, releaseDate, rating, runTime, genres }
+
+  movies.push(newMovie)
+
+  res.send(newMovie)
+}
+
+
+module.exports = { getAllMovies, searchByTitleAndDirector, createNewMovie }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const port = 3000
 const movies = require('./movies')
-const { getAllMovies } = require('./controllers/movies')
+const { getAllMovies, searchByTitleAndDirector, createNewMovie } = require('./controllers/movies')
 
 // Body Parser into JSON
 app.use(bodyParser.json())
@@ -12,41 +12,10 @@ app.use(bodyParser.json())
 app.get('/movies', getAllMovies)
 
 // Search by Director or Title
-app.get('/movies/:query', (req, res) => {
-  const qSearch = req.params.query.toLowerCase()
-
-  const movieResults = movies.filter(movie => movie.title.toLowerCase().includes(qSearch))
-
-  const directorResults = movies.filter(movie => {
-    for (director of movie.directors) {
-      if (director.toLowerCase().includes(qSearch)) {
-        return movie
-      }
-    }
-  })
-
-  const combinedResults = movieResults.concat(directorResults)
-
-  res.send(combinedResults)
-
-})
+app.get('/movies/:query', searchByTitleAndDirector)
 
 // Create New Movie
-app.post('/movies', (req, res) => {
-  const { title, directors, releaseDate, rating, runTime, genres } = req.body
-
-  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
-    return res.status(400).send('Missing Movie Data!!')
-  }
-
-  const newMovie = { title, directors, releaseDate, rating, runTime, genres }
-
-  movies.push(newMovie)
-
-  res.send(newMovie)
-})
-
-
+app.post('/movies', createNewMovie)
 
 // Set up port
 app.listen(port, () => {
